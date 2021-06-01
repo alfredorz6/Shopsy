bf30790424178da1556edb30b4a7619741744035
Alfredo Ramirez Mendez <alfredo.rz6@gmail.com>
2021-06-01 13:54:37 -0700

    user Auth first approach

---
+++ app/controllers/api/user_controller.rb
@@ -0,0 +1,17 @@
+class Api::UserController < ApplicationController
+    
+    def create
+        @user = user.new(user_params)
+
+        if @user.save
+            login(@user)
+            render "api/users/show"
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:email, :name, :password)
+end
