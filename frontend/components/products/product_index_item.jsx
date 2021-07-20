9f5f5d1d0441034846dd1378e94e88b87d34afc5
Alfredo Ramirez Mendez <alfredo.rz6@gmail.com>
2021-07-20 12:38:54 -0700

    finished details

--- frontend/components/products/product_index_item.jsx
+++ frontend/components/products/product_index_item.jsx
@@ -8,7 +8,9 @@ const ProductIndexItem = props => {
             <Link to={`/products/${props.product.id}`} className='product-index-name'>
                 {props.product.name}
             </Link>
-            <img className="product-pic" src={`${props.product.imageUrl}`} alt='product-image' /> 
+            <Link to={`/products/${props.product.id}`}>
+                <img className="product-pic" src={`${props.product.imageUrl}`} alt='product-image' />
+            </Link>
             <p className="product-price">${props.product.price}</p>
         </li>
     )
